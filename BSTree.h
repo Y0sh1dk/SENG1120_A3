288497678ec78096aa2bfe7c500b9ad51c777023
Y0sh1dk <47013245+Y0sh1dk@users.noreply.github.com>
2020-10-24 15:38:41 +1100

    Initial commit

---
+++ BSTree.h
@@ -0,0 +1,8 @@
+//
+// Created by Yosiah on 24/10/2020.
+//
+
+#ifndef SENG1120_A3_BSTREE_H
+#define SENG1120_A3_BSTREE_H
+
+#endif //SENG1120_A3_BSTREE_H
---
+++ BTNode.h
@@ -0,0 +1,8 @@
+//
+// Created by Yosiah on 24/10/2020.
+//
+
+#ifndef SENG1120_A3_BTNODE_H
+#define SENG1120_A3_BTNODE_H
+
+#endif //SENG1120_A3_BTNODE_H
